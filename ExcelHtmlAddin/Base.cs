057032881b8dbdd1f0fbd5c143c50a4b1fde48c9
szkmiyabi <led.centipede@gmail.com>
2019-10-02 22:48:56 +0900

    Baseクラスを追加

---
+++ ExcelHtmlAddin/Base.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExcelHtmlAddin
+{
+    partial class Ribbon1
+    {
+
+        //CSVファイル保存先を取得
+        private string get_csv_save_path()
+        {
+            string path = "";
+            SaveFileDialog fda = new SaveFileDialog();
+            fda.Filter = "CSVファイル(*.csv)|*.csv";
+            fda.Title = "名前を付けて保存";
+            if (fda.ShowDialog() == DialogResult.OK)
+            {
+                path = fda.FileName;
+            }
+            return path;
+        }
+
+        //TEXTファイル保存先を取得
+        private string get_txt_save_path()
+        {
+            string path = "";
+            SaveFileDialog fda = new SaveFileDialog();
+            fda.Filter = "Textファイル(*.txt)|*.txt";
+            fda.Title = "名前を付けて保存";
+            if (fda.ShowDialog() == DialogResult.OK)
+            {
+                path = fda.FileName;
+            }
+            return path;
+        }
+
+    }
+}
